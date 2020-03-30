dbc5c727620b8ee5f0b6e67fe0fb572d3f87973b
Sebasalvarez0102 <sebastian.riaza@upb.edu.co>
2020-03-29 21:28:27 -0500

    Add files via upload

---
+++ Practica 1 - Sebastian Riaza Alvarez/Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		cssmin: {
+			css: {
+				src: 'css/main.css',
+				dest: 'css/main.min.css'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+}
